61a68c06f6836694a7be3f8103b046f98d1c6235
alanmclarenbr <alan.ses@gmail.com>
2023-12-27 21:40:49 +0100

    BeerDto Validation

--- src/main/java/guru/springframework/msscbeerservice/web/model/BeerDto.java
+++ src/main/java/guru/springframework/msscbeerservice/web/model/BeerDto.java
@@ -1,6 +1,10 @@
 package guru.springframework.msscbeerservice.web.model;
 
 import guru.springframework.msscbeerservice.web.enums.BeerStyleEnum;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Null;
+import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -15,15 +19,21 @@ import java.util.UUID;
 @AllArgsConstructor
 @Builder
 public class BeerDto {
-
+    @Null
     private UUID id;
+    @Null
     private Integer version;
-
+    @Null
     private OffsetDateTime createdDate;
     private OffsetDateTime lastModifiedDate;
+    @NotBlank
     private String beerName;
+    @NotNull
     private BeerStyleEnum beerStyle;
+    @Positive
     private Long upc;
+    @Positive
+    @NotNull
     private BigDecimal price;
     private Integer quantityOnHand;
 }
