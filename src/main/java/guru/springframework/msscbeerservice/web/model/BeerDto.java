4ab342360b01421319f7b4e71828e1b422b1d143
alanmclarenbr <alan.ses@gmail.com>
2023-12-28 19:15:52 +0100

    Adding Jackson config to BeerDto

--- src/main/java/guru/springframework/msscbeerservice/web/model/BeerDto.java
+++ src/main/java/guru/springframework/msscbeerservice/web/model/BeerDto.java
@@ -1,5 +1,6 @@
 package guru.springframework.msscbeerservice.web.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import guru.springframework.msscbeerservice.web.enums.BeerStyleEnum;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -21,20 +22,32 @@ import java.util.UUID;
 public class BeerDto {
     @Null
     private UUID id;
+
     @Null
     private Integer version;
+
     @Null
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ssZ", shape = JsonFormat.Shape.STRING)
     private OffsetDateTime createdDate;
+
+    @Null
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ssZ", shape = JsonFormat.Shape.STRING)
     private OffsetDateTime lastModifiedDate;
+
     @NotBlank
     private String beerName;
+
     @NotNull
     private BeerStyleEnum beerStyle;
+
     @Positive
     private Long upc;
+
     @Positive
     @NotNull
+    @JsonFormat(shape = JsonFormat.Shape.STRING)
     private BigDecimal price;
+
     private Integer quantityOnHand;
 
 }
